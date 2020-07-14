c56648d0cd8007f626bc7e5f4610855034632682
dowhile98 <47931397+dowhile98@users.noreply.github.com>
2020-07-13 20:09:52 -0500

    CHIFU

--- 002_IO_BUTTON.X/main_button.c
+++ 002_IO_BUTTON.X/main_button.c
@@ -13,21 +13,17 @@
 #define delay_ms(x)    __delay32((Freq/1000)*x) 
 int main(void) {
     
-    /*CONFIGURACION DEL RELOJ*/
-    PLLFBDbits.PLLDIV = 0x06;           //pll div M = 8
-    CLKDIVbits.PLLPOST = 0x0;           //POST VCO: division de salida N2 = 2
-    CLKDIVbits.PLLPRE = 0x0;            //PLL PHASE N1 = 2
     /*CONFIGURACION DEL PUERTOS DE SALIDA*/
     AD1PCFGL = 0xFFFF;                      //todos los pines como digitales
     TRISBbits.TRISB0 = 0;                   //RB0 -> salida
     TRISBbits.TRISB1 = 1;                   //RA0 -> entrada
     PORTBbits.RB0 = 0;                      //se resetea el valor del pin
     while(1){
-        if(PORTBbits.RB1){
+        
+		if(PORTBbits.RB1==0){					//es verdadero sea diferente de cero
             
-            PORTBbits.RB1 =0;
-            LATBbits.LATB0 = 1;
-            delay_ms(100);
+            LATBbits.LATB0 = 1;				//prende el led conectado a RB0
+            delay_ms(100);					//retardo
         }
         else{
             LATBbits.LATB0 = 0; // si no esta presionado lo apagas
